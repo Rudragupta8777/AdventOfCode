11db755cd4cd7d7ea6bedd2e1b7a5940d76fd21e
unknown <rudragupta1204@gmail.com>
2024-12-02 01:30:41 +0530

    Day1 Solution

--- DAY_1/day1.cpp
+++ DAY_1/day1.cpp
@@ -5,16 +5,20 @@
 #include <string>
 using namespace std;
 
+// defining the number of lines as constant variable
 const int max_no_lines = 1000;
 
 int main() {
+  // Reads input file
   ifstream file("input.txt");
 
+  // Check if it's reading file or not
   if (!file) {
     cout << "File not processed" << endl;
     return 1;
   }
 
+  // create an left and right array of size 1000
   string line;
   int left_arr[max_no_lines];
   int right_arr[max_no_lines];
@@ -24,6 +28,7 @@ int main() {
     stringstream ss(line);
     string int1, int2;
 
+    // splitting the string, changing it to integer & pushing it into array
     if (ss >> int1 >> int2) {
       left_arr[count] = stoi(int1);
       right_arr[count] = stoi(int2);
@@ -32,14 +37,18 @@ int main() {
   }
   file.close();
 
+  // sorting array in ascending order
   sort(left_arr, left_arr + count);
   sort(right_arr, right_arr + count);
 
+  // calculating the difference between two array
   int sum_of_difference = 0;
   for (int i = 0; i < count; i++) {
     int difference = abs(left_arr[i] - right_arr[i]);
     sum_of_difference = sum_of_difference + difference;
   }
+
+  // printing the sum of difference between elements in a particular index
   cout << sum_of_difference << endl;
   return 0;
 }
--- DAY_1/day2.cpp
+++ DAY_1/day2.cpp
@@ -5,36 +5,43 @@
 #include <string>
 using namespace std;
 
+// defining the number of lines as constant variable
 const int max_no_lines = 1000;
 
-int main(){
+int main() {
+    // Reads input file
     ifstream file("input.txt");
 
-    if (!file){
+    // Check if it's reading file or not
+    if (!file) {
         cout << "File not processed" << endl;
         return 1;
     }
 
+    // create an left and right array of size 1000
     string line;
     int left_arr[max_no_lines];
     int right_arr[max_no_lines];
     int count = 0;
 
-    while (getline(file, line) && count < max_no_lines){
+    while (getline(file, line) && count < max_no_lines) {
         stringstream ss(line);
         string int1, int2;
 
-        if (ss >> int1 >> int2){
-            left_arr[count] = stoi(int1);
-            right_arr[count] = stoi(int2);
-            count++;
+        // splitting the string, changing it to integer & pushing it into array
+        if (ss >> int1 >> int2) {
+        left_arr[count] = stoi(int1);
+        right_arr[count] = stoi(int2);
+        count++;
         }
     }
     file.close();
 
+    // sorting array in ascending order
     sort(left_arr, left_arr + count);
     sort(right_arr, right_arr + count);
 
+    // calculating the no of times each element is repeating
     int repeated = 0;
     for (int i=0; i<count; i++){
         int n = 0;
@@ -45,6 +52,8 @@ int main(){
         }
         repeated = repeated + (left_arr[i]*n);
     }
-    cout<<repeated<<endl;
+
+    // printing the total times the elemnts are repeated
+    cout << repeated << endl;
     return 0;
 }
